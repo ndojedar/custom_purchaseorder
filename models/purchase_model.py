7d5f39a9672299aec4ec5e6006a52f92e5f1ce03
Nestor Ojeda <nojeda@adasoft.es>
2022-06-07 10:52:58 +0200

    Updated

--- models/purchase_model.py
+++ models/purchase_model.py
@@ -1,31 +1,30 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, _
+from odoo import api, models, fields, _
 
 
 class CustomPurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
     datos_entrega = fields.Many2one(
-        comodel_name='res.partner', 
+        comodel_name='res.partner',
         string='Dirección de Entrega'
     )
 
     produccion_estimada = fields.Monetary(
-        string='Producción estimada', 
-        compute='_calc_produccion_estimada', 
-        store=True, 
-        readonly=True
+        string='Producción estimada',
+        compute='_calc_produccion_estimada',
+        store=True,
     )
 
     pva_estimado = fields.Monetary(
-        string='Producción VA estimada', 
-        compute='_calc_pva_estimado', 
-        store=True, 
-        readonly=True
+        string='Producción VA estimada',
+        compute='_calc_pva_estimado',
+        store=True,
     )
 
 
+    @api.onchange('amount_untaxed')
     def _calc_produccion_estimada(self):
         for rec in self:
             val = 0.0
@@ -33,6 +32,7 @@ class CustomPurchaseOrder(models.Model):
             rec.produccion_estimada = rec.amount_untaxed * 1.5 * vforecast.sales_factor if vforecast else val
 
 
+    @api.onchange('produccion_estimada')
     def _calc_pva_estimado(self):
         for rec in self:
             val = 0.0
@@ -43,6 +43,4 @@ class CustomPurchaseOrder(models.Model):
                     rec.pva_estimado = val - rec.amount_untaxed
             else:
                 rec.pva_estimado = val
-
-
 
